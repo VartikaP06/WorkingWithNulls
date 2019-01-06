6f9f14873857d696de080495bfd82c34f34b5f40
Vartika06 <magicalvartika06@gmail.com>
2019-01-06 22:21:01 +0530

    Add project files.

---
+++ GameConsole/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameConsole
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            PlayerCharacter[] players = null;
+
+            var p1 = players?[0]?.Name;
+            var p2 = players?[1]?.Name;
+            var p3 = players?[2]?.Name;
+            Console.WriteLine(p1);
+            Console.WriteLine(p2);
+            Console.WriteLine(p3);
+            Console.ReadLine();
+        }
+    }
+}
